52fcbfe5c13b219b49cf2344cbc6bf5799d02fb3
Adam Hauze <hauze_v@protonmail.com>
2020-11-29 16:38:42 -0500

    Created initial components and added font-awesome icon packages

---
+++ src/components/Player.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = () => {
+  // Return your JSX
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="player-control">
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon
+          className="skip-forward"
+          icon={faAngleRight}
+          size="2x"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
